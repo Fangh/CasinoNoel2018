331674f95fb9d6f29c15f76c3b42f9ff7a88175d
Thomas <toto.klein@laposte.net>
2018-11-24 17:55:13 +0100

    Add the new projectile scene = TMPPro

---
+++ Assets/New Folder/Scripts/ElfTarget.cs
@@ -0,0 +1,75 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public struct MinMaxTime
+{
+    public float min;
+    public float max;
+
+    public MinMaxTime(float _min, float _max)
+    {
+        min = _min;
+        max = _max;
+    }
+}
+
+public class ElfTarget : MonoBehaviour
+{
+    [Header("Tweaking")]
+    [SerializeField] private MinMaxTime timeToMove = new MinMaxTime(1f, 5f);
+    [SerializeField] private MinMaxTime timeIdle = new MinMaxTime(1f, 5f);
+    [SerializeField] private MinMaxTime timeHidden = new MinMaxTime(1f, 5f);
+    [SerializeField] private float timeStunned = 3f;
+    [SerializeField] private float movementAmplitude = 1f;
+
+    private float originalY;
+    Sequence mySequence;
+    private Animator animatorController;
+
+    void Awake()
+    {
+        originalY = transform.position.y;
+        animatorController = GetComponent<Animator>();
+        mySequence = DOTween.Sequence();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        mySequence.AppendInterval(Random.Range(timeHidden.min, timeHidden.max))
+          .Append(transform.DOMoveY(originalY + movementAmplitude, Random.Range(timeToMove.min, timeToMove.max)))
+          .AppendInterval(Random.Range(timeIdle.min, timeIdle.max))
+          .Append(transform.DOMoveY(originalY, Random.Range(timeToMove.min, timeToMove.max)))
+          .AppendCallback(ResetAnimator)
+          .SetLoops(-1);
+    }
+
+    void ResetAnimator()
+    {
+        if (!animatorController.GetCurrentAnimatorStateInfo(0).IsName("HappyAnim"))
+        {
+            animatorController.SetTrigger("Idle");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        animatorController.SetTrigger("Hit");
+        mySequence.Pause();
+        StartCoroutine("WaitBeforeUnPause");
+    }
+
+    IEnumerator WaitBeforeUnPause()
+    {
+        yield return new WaitForSeconds(timeStunned);
+        mySequence.Play();
+    }
+}
---
+++ Assets/New Folder/WeatherAPI/WeatherAPI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+public class WeatherAPI : MonoBehaviour
+{
+    public List<WeatherObject> myList = new List<WeatherObject>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        AddWeatherInListAsync();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [ContextMenu("Add")]
+    public async void AddWeatherInListAsync()
+    {
+        WeatherObject w = await GetWeather();
+        myList.Add(w);
+    }
+
+    [Serializable]
+    public class WeatherObject
+    {
+        public int id;
+        public string name;
+        public List<Weather> weather;
+    }
+
+
+    [Serializable]
+    public class Weather
+    {
+        public int id;
+        public string main;
+        public string description;
+        public string icon;
+
+    }
+
+    public async Task<WeatherObject> GetWeather()
+    {
+        WebRequest request = WebRequest.Create(string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}", "Nantes,FR", "b865e644ced70de1ed3b9cb52f3a1f8a"));
+        WebResponse response = await request.GetResponseAsync();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+        WeatherObject info = JsonUtility.FromJson<WeatherObject>(jsonResponse);
+        return info;
+    }
+}
